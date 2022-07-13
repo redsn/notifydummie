75706f177454425aef41ca1a69a6b2f3147064c7
tkl <tluk92@gmail.com>
2022-07-12 23:02:57 -0400

    wip2302 added get and listener

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 // Dependencies
+const express = require('express');
 
 // Initialize express app
+const app = express();
 
 // configure application settings
 
 // mount middleware
 
 // mount routes
+app.get('/', (req,res) => {
+    res.send('<h1>Hello World</h1>');
+})
 
 // tell the app to listen to requests from the client
+const PORT = 3015;
+app.listen(PORT, () =>{
+    console.log(`Express is listening for reqs on ${PORT}`)
+})
