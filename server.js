723de4476d65ab095d14bec7a847ac6a81c9844d
tkl <tluk92@gmail.com>
2022-07-13 00:48:04 -0400

    0046wip: config routes and connection to mongoDB

--- server.js
+++ server.js
@@ -1,17 +1,30 @@
 // Dependencies
+require('dotenv').config();
 const express = require('express');
+const notesRouter = require('./controllers/notes');
+const mongoose = require('mongoose');
 
 // Initialize express app
 const app = express();
 
 // configure application settings
+const MongoUrl = process.env.MONGO_URL
+
+mongoose.connect(MongoUrl);
+
+mongoose.connection.on('connected', () => {
+    console.log('connected to mongoDB')
+});
 
 // mount middleware
 
 // mount routes
 app.get('/', (req,res) => {
     res.send('<h1>Hello World</h1>');
-})
+});
+
+app.use('/', notesRouter);
+
 
 // tell the app to listen to requests from the client
 const PORT = 3015;
